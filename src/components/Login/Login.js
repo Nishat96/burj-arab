d500d52ba3fd8768eb39bd018c1f80b19f04cbe2
Nishat96 <nishatnawar19@gmail.com>
2020-10-04 14:17:21 +0600

    client side is ready

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,14 +20,26 @@ const Login = () => {
             const {displayName, email} = result.user;
             const signedInUser = {name: displayName, email};
             setLoggedInUser(signedInUser);
-            history.replace(from);
-            // ...
+            storeAuthToken();
           }).catch(function(error) {
             var errorMessage = error.message;
             console.log(errorMessage);
             
           });
     }
+
+    const storeAuthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+        .then(function(idToken) {
+            // console.log(idToken);
+            sessionStorage.setItem('token', idToken);
+            history.replace(from);
+          }).catch(function(error) {
+            // Handle error
+          });
+    }
+
+
     return (
         <div>
             <h1>This is Login</h1>
